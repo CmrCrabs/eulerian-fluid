6551ee108f0a2b080169dca208ccf0f544b6ba45
CmrCrabs <zf.azam@proton.me>
2025-11-16 04:10:15 +0000

    broken?

--- src/Main.hs
+++ src/Main.hs
@@ -7,19 +7,19 @@ import qualified Data.Vector as V
 import qualified Data.Vector.Mutable as M
 
 nx :: Int
-nx = 64
+nx = 100
 
 ny :: Int
-ny = 64
+ny = 30
 
 iters :: Int -- number of iterations
-iters = 10
+iters = 60
 
 dt :: Double -- timestep
-dt = 1 / 30
+dt = 1 / 60
 
-g :: Double -- gravity
-g = -9.81
+gravity :: Double
+gravity = -9.81
 
 o :: Double -- overrelaxation
 o = 1.9
@@ -50,7 +50,7 @@ idxS :: Int -> Int -> Int
 idxS x y = x + y * nx
 
 idxU :: Int -> Int -> Int
-idxU x y = x + y * (nx+1)
+idxU x y = x + y * nx
 
 idxV :: Int -> Int -> Int
 idxV x y = x + y * nx
@@ -62,8 +62,16 @@ coord i = (i `mod` nx, i `div` nx)
 initSolids :: MutVec -> ST RealWorld ()
 initSolids sV = forM_ [0 .. nx * ny - 1] $ \i -> do
   let (x, y) = coord i
-  let isBorder = x == 0 || y == 0 || x == nx - 1 || y == ny - 1
+      isBorder = x == 0 || y == 0 || x == nx - 1 || y == ny - 1
+      (xCr, yCr) = (0.5 * fromIntegral nx * h, 0.5 * fromIntegral ny * h)
+      (xC, yC) = (fromIntegral x * h, fromIntegral y * h)
+
+  --let inCircle = dist xC yC xCr yCr <= (h * 0.1 * fromIntegral ny)
+  --M.write sV i $ if isBorder || inCircle then 0 else 1
   M.write sV i $ if isBorder then 0 else 1
+
+dist :: Double -> Double -> Double -> Double -> Double
+dist x1 y1 x2 y2 = sqrt ((x2-x1) * (x2-x1) + (y2-y1)*(y2-y1))
 
 diffuse :: MutVec -> MutVec -> ST RealWorld ()
 diffuse vV sV = forM_ [0 .. nx * ny - 1] $ \i -> do
@@ -71,7 +79,7 @@ diffuse vV sV = forM_ [0 .. nx * ny - 1] $ \i -> do
   sCell <- M.read sV i
   if sCell == 0
     then return ()
-    else M.write vV i $ oldV + g * dt
+    else M.write vV i $ oldV + gravity * dt
 
 resetPressure :: MutVec -> ST RealWorld ()
 resetPressure pressureV = forM_ [0 .. nx * ny - 1] $ \i -> do
@@ -161,14 +169,12 @@ advectVel newUV newVV uV vV sV = forM_ [0 .. nx * ny - 1] $ \i -> do
     vy1 <- M.read vV $ idxV x (y + 1)
 
     unless (sCell == 0 || sx0 == 0) $ do
-      -- check for the y > n cond
       let avgV = 0.25 * (vx0 + vx1 + vy0 + vy1)
       let xCoord = fromIntegral x * h - dt * uCell
           yCoord = fromIntegral y * h - dt * avgV
       nV <- sample vV xCoord yCoord V
       M.write newVV i nV
 
-    -- check for updating cell here
     unless (sCell == 0 || sy0 == 0) $ do
       let avgU = 0.25 * (ux0 + ux1 + uy0 + uy1)
       let xCoord = fromIntegral x * h - dt * avgU
@@ -178,9 +184,9 @@ advectVel newUV newVV uV vV sV = forM_ [0 .. nx * ny - 1] $ \i -> do
 
 sample :: MutVec -> Double -> Double -> SampleType -> ST RealWorld Double
 sample vec xC yC field = do
-  -- clamp to be within worldspace
   let h1 = 1.0 / h
 
+  -- clamp to be within worldspace
   let x = max h $ min xC (fromIntegral nx * h)
       y = max h $ min yC (fromIntegral ny * h)
 
@@ -243,21 +249,23 @@ advectSmoke uV vV sV newSmokeV = forM_ [0 .. nx * ny - 1] $ \i -> do
         vC = vCell + vy1 * 0.5
 
     let xStep = fromIntegral x * h + 0.5 * h - dt * uC
-        yStep = fromIntegral y * h + 0.5 * h + dt * vC
+        yStep = fromIntegral y * h + 0.5 * h - dt * vC
 
     newS <- sample newSmokeV xStep yStep S
     M.write newSmokeV i newS
 
 windTunnel :: MutVec -> MutVec -> ST RealWorld ()
 windTunnel uV smokeV = do
-  let windU = 0.5
-      smokeAmount = 0.5
+  let windU = 3.0
+      smokeAmount = 1.0
+      vertOffset = floor (fromIntegral ny * 0.45)
 
-  forM_ [(ny `div` 3)..(ny - (ny `div` 3))] $ \y -> do
-    currentU <- M.read uV (idxS 1 y)
+  forM_ [vertOffset .. (ny - vertOffset)] $ \y -> do
+    currentU <- M.read uV (idxU 1 y)
     M.write uV (idxU 1 y) (currentU + windU)
     currentSmoke <- M.read smokeV (idxS 1 y)
     M.write smokeV (idxS 1 y) (currentSmoke + smokeAmount)
+
 
 main :: IO ()
 main = do
@@ -274,10 +282,10 @@ main = do
     initSolids sV
     pure (uV, vV, newUV, newVV, sV, pressureV, smokeV, newSmokeV)
 
-  forM_ [0..30] $ \i -> do
+  forM_ [(0 :: Int)..100] $ \i -> do
     stToIO $ do
       windTunnel uV smokeV
-      diffuse vV sV
+      --diffuse vV sV
       resetPressure pressureV
       project uV vV sV pressureV
       extrapolateBorder uV vV
@@ -298,39 +306,25 @@ main = do
         maxP = V.maximum $ frozenPressure
 
     let renderpx x y =
-          --smokeColor (frozenS V.! idxS x y) (frozenSmoke V.! idxS x y)
-          sciColor (frozenS V.! idxS x y) (frozenPressure V.! idxS x y) minP maxP
+          sciSmokeColor (frozenS V.! idxS x y) (frozenPressure V.! idxS x y) minP maxP (frozenSmoke V.! idxS x y)
 
     writePng ("frames/frame_" ++ show i ++ ".png") $
       generateImage renderpx nx ny
     putStrLn ("rendered frame " ++ show i)
 
-
--- ERRORS
--- when nx != ny checquering
-
-sciColor :: Double -> Double -> Double -> Double -> PixelRGB8
-sciColor 0 _ _ _ = PixelRGB8 255 0 0
-sciColor _ p minP maxP =
+sciSmokeColor :: Double -> Double -> Double -> Double -> Double -> PixelRGB8
+sciSmokeColor 0 _ _ _ _ = PixelRGB8 255 0 0
+sciSmokeColor _ p minP maxP smokeV =
   let eps = 1e-4
       d = maxP - minP
-      v = if d == 0 then 0.5 else (max minP (min p (maxP - eps)) - minP) / d
+      val = if d == 0 then 0.5 else (max minP (min p (maxP - eps)) - minP) / d
       m = 0.25
-      num = floor (v / m) :: Int
-      s = (v - fromIntegral num * m) / m
+      num = floor (val / m) :: Int
+      sc = (val - fromIntegral num * m) / m
       (r,g,b) = case num of
-        0 -> (0, s, 1)
-        1 -> (0, 1, 1 - s)
-        2 -> (s, 1, 0)
-        _ -> (1, 1 - s, 0)
-      to8 x = floor (255 * x)
+        0 -> (0, sc, 1)
+        1 -> (0, 1, 1 - sc)
+        2 -> (sc, 1, 0)
+        _ -> (1, 1 - sc, 0)
+      to8 x = floor (255 * x * min 1.0 smokeV)
   in PixelRGB8 (to8 r) (to8 g) (to8 b)
-
-
-
-
-smokeColor :: Double -> Double -> PixelRGB8
-smokeColor 0 _ = PixelRGB8 255 0 0
-smokeColor _ smokeV =
-  let c = floor (255 * smokeV)
-   in PixelRGB8 c c c
